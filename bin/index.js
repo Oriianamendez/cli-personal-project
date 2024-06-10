d77149b516942994e64a372ec9bab4b64003a6a3
Oriana Mendez <egleemendez@gmail.com>
2024-06-10 09:55:55 +0200

    Created a message in a box with color

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
+const chalk = require("chalk");
+const boxen = require("boxen");
 
-console.log("Hello World!");
+const greeting = chalk.white.bold("Hello World! =)");
+const boxOptions = {
+  padding: 1,
+  margin: 1,
+  borderStyle: "round",
+  borderColor: "green",
+};
+const mesBox = boxen(greeting, boxOptions);
+
+console.log(mesBox);
