4397625fa0c440a98654754d11e11db6852e5eef
Oriana Mendez <egleemendez@gmail.com>
2024-06-10 10:05:58 +0200

    Made greeting funtional

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,25 @@
 #! /usr/bin/env node
 const chalk = require("chalk");
 const boxen = require("boxen");
+const yargs = require("yargs");
 
-const greeting = chalk.white.bold("Hello World! =)");
+const options = yargs
+  .usage("Usage: -n <name>")
+  .option("n", {
+    alias: "name",
+    describe: "Your name",
+    type: "string",
+    demandOption: true,
+  }).argv;
+
+const greeting = chalk.white.bold(`Hello, ${options.name}! =)`);
 const boxOptions = {
   padding: 1,
   margin: 1,
   borderStyle: "round",
   borderColor: "green",
 };
+
 const mesBox = boxen(greeting, boxOptions);
 
 console.log(mesBox);
